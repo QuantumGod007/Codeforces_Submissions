ce88e2477cdf8d401d520a61dfafe714e30389b1
Anvith Shetty <anvith320@gmail.com>
2026-08-22 23:44:55 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int m,n;
+    cin >> m >> n;
+    int total = m * n;
+ 
+    cout << total / 2 << '
+';
+    
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+    
+    return 0;
+}
