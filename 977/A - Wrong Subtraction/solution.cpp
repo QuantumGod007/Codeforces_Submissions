6c75a4715403cc821ac10ff127141492662d160b
Anvith Shetty <anvith320@gmail.com>
2026-08-22 23:42:46 +0530

    Add Wrong Subtraction [A] from Codeforces

---
+++ 977/A - Wrong Subtraction/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int n,k;
+    cin >> n >> k;
+    while(k--) {
+        if((n%10) > 0) {
+            n--;
+        }
+        else {
+            n /= 10;
+        }
+    }
+ 
+    cout << n << '
+';
+    
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    
+    return 0;
+}
