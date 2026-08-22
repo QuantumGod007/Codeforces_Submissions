b7be08d92f7fa2b2f0765184fc4ef05c0de90b45
Anvith Shetty <anvith320@gmail.com>
2026-08-22 23:45:52 +0530

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    string a,b,c;
+    cin >> a >> b >> c;
+    string f;
+    f.push_back(a[0]);
+    f.push_back(b[0]);
+    f.push_back(c[0]);
+ 
+    cout << f << '
+';
+ 
+ 
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
