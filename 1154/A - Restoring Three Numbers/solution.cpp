bb7c9bfc4287787a09d5f09b80857330334ec918
Anvith Shetty <anvith320@gmail.com>
2026-08-22 23:42:21 +0530

    Add Restoring Three Numbers [A] from Codeforces

---
+++ 1154/A - Restoring Three Numbers/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    vector<int> v(4);
+    for(int i=0;i<4;i++) {
+        cin >> v[i];
+ 
+    }
+    sort(v.begin(),v.end());
+    cout << v[3] - v[0] << " " << v[3] - v[1] << " " << v[3] - v[2];
+    
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
