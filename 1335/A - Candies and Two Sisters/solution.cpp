1e9bb7c744fc7c3f45abc0857f2cba3cf053bf6f
Anvith Shetty <anvith320@gmail.com>
2026-08-22 23:42:24 +0530

    Add Candies and Two Sisters [A] from Codeforces

---
+++ 1335/A - Candies and Two Sisters/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+void solve() {
+    ll n;
+    cin >> n;
+    cout << (n+1)/2 - 1 << "
+";
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
