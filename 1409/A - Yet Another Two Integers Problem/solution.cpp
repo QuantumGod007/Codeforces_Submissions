5add09a09eda177a81808d264324fea543d64435
Anvith Shetty <anvith320@gmail.com>
2026-08-22 23:48:02 +0530

    Add Yet Another Two Integers Problem [A] from Codeforces

---
+++ 1409/A - Yet Another Two Integers Problem/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int a,b;
+    cin >> a >> b;
+    int diff = abs(a-b);
+    cout << (diff+9)/10 << '
+';
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while(t--) {
+        solve();
+    }
+ 
+    return 0;
+}
