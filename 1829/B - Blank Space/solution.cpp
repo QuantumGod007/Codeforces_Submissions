dabcfa0fe83c9f146974536456ea3a448e45f36a
Anvith Shetty <anvith320@gmail.com>
2026-08-22 23:44:31 +0530

    Add Blank Space [B] from Codeforces

---
+++ 1829/B - Blank Space/solution.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    int maxBlank = INT_MIN;
+ 
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+ 
+    int cnt = 0;
+    for (int i=0;i<n;i++) {
+        if(arr[i] == 0) {
+            cnt++;
+        }
+        else {
+            cnt = 0;
+        }
+        maxBlank = max(maxBlank,cnt);
+    }
+ 
+    cout << maxBlank << '
+';
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
