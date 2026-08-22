6a7c29efaea3686d7516f127dd95c886687a947b
Anvith Shetty <anvith320@gmail.com>
2026-08-22 23:45:56 +0530

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void solve() {
+    int n;
+    cin >> n;
+    for(int i=1;i<=n-1;i++) {
+        if(i%2!=0) {
+            cout << "I hate that" << " ";
+        }
+        else {
+            cout << "I love that" << " ";
+        }
+    }
+    if(n%2==0) {
+        cout << "I love it";
+    }
+    else {
+        cout << "I hate it";
+    }
+    
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    solve();
+    
+    return 0;
+}
