e63269d90eb0f2bf082b028a9baa0113fb482a4f
Anvith Shetty <anvith320@gmail.com>
2026-08-22 23:38:35 +0530

    Add Shizuku Hoshikawa and Farm Legs [A] from Codeforces

---
+++ 2171/A - Shizuku Hoshikawa and Farm Legs/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        if (n % 2 != 0) {
+            cout << 0 << '
+';
+        } else {
+            cout << (n / 4) + 1 << '
+';
+        }
+    }
+    return 0;
+}
